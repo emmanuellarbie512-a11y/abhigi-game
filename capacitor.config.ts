6b01384042babd0be84f3cc3e407906a6c0f3354
emmanuellarbie512-a11y <emmanuellarbie512@gmail.com>
2026-08-18 02:52:13 +0000

    Initial commit with Capacitor and gesture controls

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.abhigi.game',
+  appName: 'Abhigi Game',
+  webDir: 'www'
+};
+
+export default config;
